215aba4fe27be275e7fdc128601ad09901833feb
Tatsushi-Nakamura <tatsushi.nakamura@mvrck.co.jp>
2019-08-26 12:16:48 +0900

    add props page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Redirect, Route, RouteComponentProps, Switch } from 'react-router-dom';
 import TopComponent from './Top';
 import StateComponent from './State';
+import PropsComponent from './Props';
 import Frame from '../components/templates/Frame';
 
 type TOutter = RouteComponentProps;
@@ -14,6 +15,7 @@ const Pages: React.FC<PagesProps> = () => {
   return (
     <Frame>
       <Switch>
+        <Route component={PropsComponent} exact={true} path="/props" />
         <Route component={StateComponent} exact={true} path="/state" />
         <Route component={TopComponent} exact={true} path="/top" />
         <Route exact={true} path="/" render={renderRedirect} />
