21414405ba41af7e61bd746184d434a7ce68b191
Tatsushi-Nakamura <tatsushi.nakamura@mvrck.co.jp>
2019-08-26 12:03:08 +0900

    remove comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
-// import { ConnectedRouter } from 'connected-react-router';
-// import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-// import './index.css';
 import PagesComponent from './pages';
 import * as serviceWorker from './serviceWorker';
-
-// const history = createBrowserHistory();
 
 ReactDOM.render(
   <BrowserRouter>
@@ -16,7 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
